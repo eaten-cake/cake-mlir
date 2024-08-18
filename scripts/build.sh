db6b39c55b19f2a8863cf5c8e0a4b840d23efad2
yrx <421626388@qq.com>
2024-08-18 23:04:22 +0800

    using custom llvm version

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,15 @@ mkdir -p $SCRIPT_DIR/../build
 
 cd $SCRIPT_DIR/../build
 
+export LLVM_INSTALL_DIR=/home/yrx/llvm-project/build
+
 cmake .. -G Ninja \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
     -DCMAKE_INSTALL_PREFIX=~/install \
     -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+    -DMLIR_DIR="$LLVM_INSTALL_DIR/lib/cmake/mlir/" \
+    -DLLVM_DIR="$LLVM_INSTALL_DIR/lib/cmake/llvm/" \
 
 ninja -j4 install
 
